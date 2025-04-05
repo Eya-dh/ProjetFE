b0cb528d6fd4efcebe831d90b3a4a4c8dea56953
Eya-dh <eyadhouib6@gmail.com>
2025-04-05 20:30:32 +0100

    deleted files

--- launch/hospital_cam.py
+++ launch/hospital_cam.py
@@ -1,6 +1,6 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, TimerAction,ExecuteProcess
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
@@ -33,7 +33,7 @@ def generate_launch_description():
     world_file_path = os.path.join(
         get_package_share_directory('robot_hospital_logistics'),
         'worlds',
-        'hospital3.world'
+        'newhospital.world'
     )
 
     # Vérifier si le fichier de monde existe
@@ -101,6 +101,7 @@ def generate_launch_description():
         parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}],
         output='screen'
     )
+
 
     return LaunchDescription([
         declare_use_sim_time,  # Ajouter l'argument pour use_sim_time
